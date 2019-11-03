7679f0646241eda277482ef50a9056037cf2ad43
philzook58 <philzook58@gmail.com>
2019-11-02 23:08:36 -0400

    some linrel notes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# lqr
+# Convex Programming in Haskell Stuff
+
+I feel pulled in multiple directions. I want to wait to write a blog post until the ideas in it have some level of completion. But sometimes that level never comes. My attention may wander off. And yet, I feel I often have some interesting half built things to show.
+I have many loves in my life, but two of them are Haskell and Convex Programming.
+
+In a previous post, I talked a bit about how reducing convex programming to questions about cones leads to elegance and simplification.
+
+There are a couple of approaches to well-typed vectors in Haskell. One of them is to consder an f :: * -> * parameter as characterizing a vector space. We intend to fill in this with things like V4.
+data V4 a = V4 a a a a 
+A vector space can be consdiered $R^n$. The f shape is sort of the $-^n$ part. The part that tells us the shape/ size of the vector space. We can then choose to fill it in, making is a vector space over Doubles or Complex Doubles or something more exotic.
+The fundamental construct we want to talk about for cones is the Ray, rather than the vector or point. A Ray is a direction. We can represent it with a vector, if we ignore the magnitude of the vector or if we choose to always work with normalized vectors (which is less elegant really).
+A cone is a set that is closed under addition and non negative scalar multiplication. It is a convex set of rays.
+The dual space to rays are the halfspace cones. They can be naturally parametrized also by vectors (basically the vectors normal to the planes pointing in the direction of halfspace you want). Any ray that has positive dot product with this vector is in the halfspace, which gives a simple test.
+
+Polytopes have (at least) two natural representations. They can be expressed as a sum of generator rays (the corners of the polytope) or as the set obeying a set of halfplane constraints. The first is called the V representation and the latter the H representation. The two are highly interconnected by duality.
+
+The difficulty and method for solving these qeustions can depend strongly on the representation you have
+
+What are geoemtrically interesting questions to ask. 
+1. Whether a ray is in a cone.
+   + Easy given HRep. Just do all the dot products
+   + 
+2. Finding a ray that is in a cone
+  - Any ray or an extremal ray? Extremal according to what?
+  - Projecting to a cone. There are metrics available. The cosine metric. The Chord metric. Others? They feel a bit goofy. They feel far less elegant than the quadratic metric of a vector space. The dot product is a somewhat natural thing to talk about, but by design. Abs metric? You can use the unit plane. and put a metric on it. The cone space is a relative of projective space. which has similar problems. The cross ratio? Logarithm of dot product? Maybe you do need a plane to work on to have a reasonable deifition of extremal. Etremal with respect to a cone generalized inequality on the euclidean space. Extremal with respect to another cone? But a particular kind of cone. It contains (0,0,0,1)? A projection excluding (0,0,0,1)
+
+
+4. Convert VRep to HRep and vice versa
+   + take every possible choice out of set. Do linear solve. Check if linear solve matches everything else.
+5. Intersections
+  + Alternating Projecction. The most primitive and obvious.  
+  + ADMM. 
+6. Minkowski Sums = Convex Hull
+7. Projection
+8. Pruning Redundancy
+9. Cone Containment Testing.
--- src/LinRel.hs
+++ src/LinRel.hs
@@ -80,3 +80,320 @@ par
 
 
 -}
+
+
+{-
+
+import numpy as np
+
+
+def meet(a,b):
+    pass
+def compose(a,b): # a after b
+    assert(a.inN == b.outN)
+
+    combo = np.block([[a.constraints, np.zeros((a.constraints.shape[0] , b.inN) )     ],
+                      [np.zeros((b.constraints.shape[0] , a.outN)) ,      b.constraints]])
+    print(combo)
+    gens = LinRel(a.inN + a.outN, b.outN, gens=combo).gens
+    print("gens",gens) 
+    gens = np.vstack((gens[:a.outN, :], gens[-b.inN: , :]) )
+    print(gens)
+    return LinRel(a.outN, b.inN, gens=gens)
+def top(outN, inN):
+    return LinRel(outN, inN, constraints = np.array([[]]))
+def bottom(outN, inN):
+    return LinRel(outN, inN, gens = np.array([[]]))
+
+def converse(a):
+    return LinRel(a.inN, a.outN, np.hstack((a.constraints[:, a.inN:], a.constraints[:, :a.inN])))
+def complement(a):
+    return LinRel(a.outN, a.inN, constraints = a.gens.T.conj())
+
+def right_div(a,b):
+    pass # return complement( compose(a, complement(b)) ) # something like this
+def inclusion(a,b):
+    
+    s = a.constraints @ b.gens
+    np.all(a.constraints @ b.gens <= tol )
+
+    if rcond is None:
+        rcond = np.finfo(s.dtype).eps * max(max(a.shape), max(b.shape))
+        tol = max(np.amax(a), np.amax(b))
+        tol = np.amax(s) * rcond
+def fromMat(mat):
+    (outN, inN) = mat.shape
+    return LinRel(inN, outN,constraints = np.hstack((mat,-np.eye(outN))))
+def id(N):
+    return fromMat(np.eye(N))
+# make 0,1 first index for in/out? Oh, but then in out have to be same size.
+# A[0, ...] @ x + A[1, ...] @ y = 0   
+# Then I can form the kron of linear relations. 
+# store sperate A B matrices? A @in + B @ out
+class LinRel():
+    def __init__(self, outN, inN, constraints = None, gens = None, rcond=None):
+        #assert(inN <= constraints.shape[1])
+        self.inN = inN
+        self.outN = outN
+        if constraints is not None: #baiscally scipy.linalg.null_space
+            u, s, vh = np.linalg.svd(constraints, full_matrices=True)
+            M, N = u.shape[0], vh.shape[1]
+            if rcond is None:
+                rcond = np.finfo(s.dtype).eps * max(M, N)
+            tol = np.amax(s) * rcond
+            num = np.sum(s > tol, dtype=int)
+            self.gens = vh[num:,:].T.conj()
+            self.constraints = vh[:num,:]
+        if gens is not None: #basically scipy.linalg.orth
+            u, s, vh = np.linalg.svd(gens, full_matrices=True)
+            M, N = u.shape[0], vh.shape[1]
+            if rcond is None:
+                rcond = np.finfo(s.dtype).eps * max(M, N)
+            tol = np.amax(s) * rcond
+            num = np.sum(s > tol, dtype=int)
+            self.gens = u[:, :num]
+            self.constraints = u[:, num:].T.conj()
+
+    def shape(self):
+        return (self.outN, self.inN)
+    def size(self):
+        return self.outN + self.inN
+    # operator overloadings
+    def __matmul__(a,b):
+        return compose(a,b)
+    def __invert__(a): # ~
+        return complement(a)
+    def __or__(a,b): # an argument could be made for + and *
+        return join(a,b)
+    def __and__(a,b):
+        return meet(a,b)
+    def __sub__(a,b):
+        return  (a) & (-b)
+    def __le__(a,b): # Are the others automatic?
+        return inclusion(a,b)
+    def __str__(self):
+        return " Constraints: \n%s, \nGens:\n %s\n" % (str(self.constraints), str(self.gens))
+
+
+
+
+ex = LinRel(1,2, np.array([[3,4,0]]))
+e2 = LinRel(2,1, np.array([[3,4,0]]))
+
+assert(np.all(LinRel(1,2, ex.constraints).constraints == ex.constraints) )
+print( ex.constraints @ ex.gens)
+assert(np.all( np.abs(ex.constraints @ ex.gens) <= 1e-15) )
+print(ex @ e2)
+print(e2 @ ex)
+print(e2 @ id(e2.inN))
+
+'''
+Quadratic optimization can be mixed in.
+Quad && LinRel
+
+AffineRel = maintain homgenous coord, or always insert 1 -1 keeping homoeg coord
++ discrete? Maintain a couple copies of 
+
+
+'''
+
+
+Linear relations
+hrep - Ax = 0
+or
+vrep - y = sum x_i
+
+hrep <-> vrep = row echelon
+
+in and out variables. In and out subspaces.
+in : [] - list of indices
+out : []
+
+in is a vrep of input space.
+
+in = projection/injection matrix n x d
+out = projection/injection matrix. (d-n) x d
+in * out = 0. orthogonal
+
+auxiliary variables allowed
+
+compose relations
+in1 out1
+in2 out2
+
+in = d x n
+stack out1 and in2 into matrix. with them equal.
+
+np.hstack(A1, out1 - in2, A2)
+in = no.hatck(in, zeores)
+in = no.hatck(zeores, out)
+
+drect sum as monoidal product
+block([ A, 0  ],
+      [ 0, A  ])
+in = [in1, in2]
+out = [out1, out2]
+
+converse = flip in out
+
+meet = combine the two constraint matrices
+join = convert? combine in out?
+
+1-d as unit object
+<= is subspace ordering
+
+negation = orthogonalization
+division => 
+
+
+a linear problem is a linear relation of 1-d.
+
+use in1 and out2 as new in/out
+
+fan 
+snd(30,10) = project bottom 10 = idetnity matrix stacked.
+fst(30, 10) project top 10
+id(20)
+id(n) = LinRel(np.zeros(0,2*n), [zeros, eye], [eye, zero] ) 
+id(n) = LinRel((0,n), eye(n), eye(n)  )
+        LinRel [I, -I], [I, 0], [0,I]
+
+"internal" space
+class LinRel():
+    def __init__(A, in, out):
+
+svd(in * A , smallest )
+1 - A*A
+
+Ax + By = 0
+
+vstack adds constraints
+hstack adds variables
+
+def idRel(n):
+  return LinRel(sparse.lil_matrix((0,n)), n, n)
+def mapRel(A):
+  (r,c) = A.shape
+  newA = sparse.hstack(A, - sparse.eye(r))
+  return LinRel(newA, r, c) 
+
+class LinRelV():
+class LinRelH():
+
+class LinRel():
+  def init(self,A, in, out):
+    self.A = A
+    self.in = in
+    self.out = out
+  def compose(self, b):
+    assert(self.out == b.in, "Shapes must match")
+    
+    i = sparse.eye(b.in)
+    cons = sparse.hstack([0, i, -i, 0])
+    ina = self.A[:,:self.in]
+    auxa = self.A[:,self.in:-self.out]
+    outa = self.A[:,-self.out:]
+    inb = b.A[:,:b.in]
+    auxb = b.A[:,b.in:-b.out]
+    outb = b.A[:,-b.out:]
+
+    newA = sparse.bmat(  [[ina, auxa, outa, 0, 0,    0],
+                          [0  , 0   , i,   -i, 0 ,   0]
+                          [0,  0,   0,    inb, auxb,outb]])
+    LinRel(newA, self.in, b.out)
+  def meet(self,b):
+    #hmm. I suppose we acutlaly should split the thing apart again
+    assert(self.in == b.in)
+    assert(self.out == b.out)
+    assert()
+    newA = sparse.vstack([self.A, b.A])
+    return ()
+  def complement(self):
+    linalg.svd(self.A)
+    return LinRel(get_nonzeroeigs)
+  def __negate__(self):
+    return self.complement()
+  def rdiv(self):
+  def transpose():
+    self.converse()
+  def T():
+    self.converse()
+  
+  # the svd gives you most of what you need?
+  def inclusion():
+    x = linalg.nullspace(self.A)
+    return b.A @ x == 0 #check that every generator is in. Makes sense. Except numrically is trash.
+  def __leq__(self):
+    self.inclusion(b)
+
+  
+
+  def converse(rel):
+      newA = np.hstack( [ rel.A[:,-rel.out: ] , rel.A[:,rel.in:-rel.out], rel.A[:,:rel.in ] ])
+      return LinRel(newA, rel.out, rel.in)
+
+
+compose(A)
+linalg.nullspace(A)
+range?
+
+
+
+(cons1,d1) = self.A.shape
+(cons2,d2) = b.A.shape
+constrain = np.hstack 
+newA = sparse.vstack 
+
+
+using bigM I can encode the complement of a H-polytope 
+but then what?
+I do it again I guess?
+
+
+complementation of relation ->
+At least one must be inverted.
+
+polytope inclusion
+-> search for point not in B.
+Or, do sandardinni encoding
+
+Really It should generate new variables for an instantiation.
+Snd should not reuse the same variables every time.
+class PolyRel()
+  invars = []
+  constraints = [] # store >= values, not full constraints?
+  outvars = []
+  def __init__():
+    all fresh variables
+  def compose(self,b):
+    PolyRel(self.constraints + self., invars = outvars )
+  def complement():
+    zs = []
+    for c in constraints:
+      z, constraints = reify(c)
+      # z = cvx.Variable(1, boolean=True) # actually make same shape as c
+      # c += c + M * z
+    sum(zs) >= 1 # one or more constraints is disatisfied.
+  def rdiv():
+
+yeah. We should use a dsl, compile it, then encode it.
+data Rel a b where
+  Compose ::
+  Complement ::
+  Converse ::
+
+
+relu
+
+l1 >= 0
+l2 >= 0
+l1 <= M * z
+l2 <= M * (1 - z)
+x = lambda1 - lambda2
+y = lambda1
+
+Maybe insetado f subspaces, we should be thinking ellipses and svd.
+
+
+
+-}
